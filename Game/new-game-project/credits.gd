5641385021fe7c0caada31a7ab43e035c42a9275
Rory Poulter <rorypoulter2@gmail.com>
2025-03-30 23:31:20 +0100

    Add esc keybind to return to title page from credits (#1)

--- Game/new-game-project/credits.gd
+++ Game/new-game-project/credits.gd
@@ -3,3 +3,8 @@ extends Control
 
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://title_screen.tscn")
+
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("ui_cancel"):
+		get_tree().change_scene_to_file("res://title_screen.tscn")
--- Game/new-game-project/title_screen.gd
+++ Game/new-game-project/title_screen.gd
@@ -3,9 +3,11 @@ extends Control
 
 func _on_start_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://world.tscn")
-	pass # Replace with function body.
 
 
 func _on_controls_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://control.tscn")
-	pass # Replace with function body.
+
+
+func _on_credits_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://credits.tscn")
