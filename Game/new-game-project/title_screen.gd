0d190664089c55e1d4ab770861145d085da6a7e7
Rory Poulter <rorypoulter2@gmail.com>
2025-04-03 13:34:19 +0100

    Change totle screen to open player selection (#1)

--- Game/new-game-project/title_screen.gd
+++ Game/new-game-project/title_screen.gd
@@ -2,7 +2,7 @@ extends Control
 
 
 func _on_start_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://world.tscn")
+	get_tree().change_scene_to_file("res://player_selection.tscn")
 
 
 func _on_controls_button_pressed() -> void:
