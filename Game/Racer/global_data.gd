99893d2847f4188ecbc0347ec125405bedf27a42
Rory Poulter <rorypoulter2@gmail.com>
2025-04-11 14:19:33 +0100

    Update global variables to include number of players, number of laps and paths to racers (#1)

--- Game/Racer/global_data.gd
+++ Game/Racer/global_data.gd
@@ -1,3 +1,7 @@
 extends Node
 
+var two_player: bool
 var game_track_path: String
+var number_of_laps: int
+var player_one_car_path: String
+var player_two_car_path: String
--- Game/Racer/main-menu/player-selection/player_selection.gd
+++ Game/Racer/main-menu/player-selection/player_selection.gd
@@ -5,3 +5,11 @@ extends Control
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_cancel"):
 		get_tree().change_scene_to_file("res://main-menu/title-screen/title_screen.tscn")
+
+
+func _on_single_player_button_pressed() -> void:
+	GlobalData.two_player = false
+
+
+func _on_multi_player_button_pressed() -> void:
+	GlobalData.two_player = true
